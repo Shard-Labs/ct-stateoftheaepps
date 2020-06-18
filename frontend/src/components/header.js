c2c6fe59112121c4598bc30481677dc5ff80632c
igor-lid <onaj.igor@gmail.com>
2020-06-18 16:22:02 +0200

    move to frontend

---
+++ frontend/src/components/header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import DesktopLogo from "../assets/images/logo.png";
+import MobileLogo from "../assets/images/logo_mobile.png";
+
+function Header() {
+  return (
+    <header class="header">
+      <img class="logo-desktop" src={DesktopLogo} alt="logo" />
+      <img class="logo-mobile" src={MobileLogo} alt="logo" />
+    </header>
+  );
+}
+
+export default Header;
--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,5 +1,25 @@
-import React from "react"
+import React from "react";
+import Header from "../components/header";
+import Footer from "../components/footer";
+import "../styles/styles.scss";
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <>
+      <Header />
+
+      <main class="main">
+        <section class="description">
+          <h1 class="title-main">List of most used Æternity æpps</h1>
+          <p class="description-text">
+            Check to see the progress and development of æpps and Æternity
+            blockchain ecosystem. List is updated daily based on the number of
+            transactions in the last 24 hours.
+          </p>
+        </section>
+      </main>
+
+      <Footer />
+    </>
+  );
 }
