1b8b7f5b02529d148d9381be70a5b8af847b06d5
anamst <ana.milic.strkalj@gmail.com>
2020-06-18 15:22:30 +0200

    frontend folder

---
+++ frontend/src/components/footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer class="footer">
+      <p class="text-footer">Powered by shardlabs.io</p>
+    </footer>
+  );
+}
