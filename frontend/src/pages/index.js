c58367e39b1e0d720dffe22d7a890d4976620a51
anamst <ana.milic.strkalj@gmail.com>
2020-06-24 11:43:25 +0200

    list items flexbox fixed

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -11,6 +11,20 @@ function Home() {
           blockchain ecosystem. List is updated daily based on the number of
           transactions in the last 24 hours.
         </p>
+        <ul class="list">
+          <li class="list-row">
+            <span class="item-1">1.</span>
+            <img
+              class="item-2"
+              src="https://jelly.market/static/logo-white-174143b2b447566a6eb46e19cbe10b7b.svg"
+              alt="image"
+            />
+            <a class="item-3" href="https://jelly.market/">
+              JellySwap
+            </a>
+            <span class="item-4">12345</span>
+          </li>
+        </ul>
       </section>
     </Layout>
   );
