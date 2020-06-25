5cb050f77e2eb123bb704f11c06ff7aa660d8dc8
Karlo <karlomajer1@gmail.com>
2020-06-25 14:41:58 +0200

    Update Dapp model with website_url and description fields

--- backend/config/contracts.ts
+++ backend/config/contracts.ts
@@ -1,17 +1,23 @@
 export default [
   {
-    name: 'CryptoTask',
-    contract_address: 'ct_DeRuk3os1AaoiNtpiHnX5GnUHCVhqgZP9uQnEdx1UzNMq65Pf',
-    image_name: 'cryptotask.svg',
+    name: "CryptoTask",
+    website_url: "https://www.cryptotask.org/",
+    description: "Placeholder description",
+    contract_address: "ct_DeRuk3os1AaoiNtpiHnX5GnUHCVhqgZP9uQnEdx1UzNMq65Pf",
+    image_name: "cryptotask.svg",
   },
   {
-    name: 'SuperHero',
-    contract_address: 'ct_2AfnEfCSZCTEkxL5Yoi4Yfq6fF7YapHRaFKDJK3THMXMBspp5z',
-    image_name: 'superhero.svg',
+    name: "SuperHero",
+    website_url: "https://superhero.com/",
+    description: "Placeholder description",
+    contract_address: "ct_2AfnEfCSZCTEkxL5Yoi4Yfq6fF7YapHRaFKDJK3THMXMBspp5z",
+    image_name: "superhero.svg",
   },
   {
-    name: 'Jelly Swap',
-    contract_address: 'ct_jmRkfpzmn7KZbXbkEL9wueJkb1vaFzMnVFJMFjAnJgj1CTtQe',
-    image_name: 'jelly.svg',
+    name: "Jelly Swap",
+    website_url: "https://jelly.market/",
+    description: "Placeholder description",
+    contract_address: "ct_jmRkfpzmn7KZbXbkEL9wueJkb1vaFzMnVFJMFjAnJgj1CTtQe",
+    image_name: "jelly.svg",
   },
 ];
--- backend/types.ts
+++ backend/types.ts
@@ -1,5 +1,7 @@
 interface Contract {
   name: string;
+  website_url: string;
+  description: string;
   contract_address: string;
   image_name: string;
 }
