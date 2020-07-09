e0b0700c674389ce80d9cced564e02ee288fa16c
anamst <ana.milic.strkalj@gmail.com>
2020-07-09 12:54:52 +0200

    react-helmet plugin added

--- frontend/src/components/layout/layout.js
+++ frontend/src/components/layout/layout.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import Header from './header';
 import Footer from './footer';
+import { Helmet } from 'react-helmet';
 
 import '../../styles/styles.scss';
 
 function Layout({ children }) {
   return (
     <>
+      <Helmet>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"
+        />
+      </Helmet>
       <Header />
       <main className="main">{children}</main>
       <Footer />
