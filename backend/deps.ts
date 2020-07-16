ad5cb6d6c9e8cca2779a744b46d2f7d6833fe90a
Karlo Majer <karlomajer1@gmail.com>
2020-07-16 09:23:34 +0200

    Update deno dependencies to fixed versions

--- backend/deps.ts
+++ backend/deps.ts
@@ -1,7 +1,6 @@
-export { Application } from 'https://deno.land/x/oak/mod.ts';
-export { config } from 'https://deno.land/x/dotenv/mod.ts';
-export { Database } from 'https://deno.land/x/denodb/mod.ts';
+export { Application, Router } from 'https://deno.land/x/oak@v5.3.1/mod.ts';
+export { config } from 'https://deno.land/x/dotenv@v0.5.0/mod.ts';
+export { Database } from 'https://deno.land/x/denodb@v1.0.4/mod.ts';
 export { Cron } from 'https://deno.land/x/cron/cron.ts';
-export { Router } from 'https://deno.land/x/oak/mod.ts';
-export { Base64 } from 'https://deno.land/x/bb64/mod.ts';
+export { Base64 } from 'https://deno.land/x/bb64@1.0.0/mod.ts';
 export { oakCors } from 'https://deno.land/x/cors/mod.ts';
